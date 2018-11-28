28a9adc9cb644018e692a51a7b58ede06db4171a
Nguyen Tran <kevtran07@gmail.com>
2018-11-29 00:21:42 +0200

    working on post details page

--- index.js
+++ index.js
@@ -36,6 +36,10 @@ app.get('/upload', (_, res) => {
 	res.sendFile('upload.html', clientRoot);
 });
 
+app.get('/post/:id', (_, res) => {
+	res.sendFile('view-post.html', clientRoot);
+});
+
 //use this for any other undefined routes
 app.use((req, res, next) => {
 	const error = new Error('Not found');
