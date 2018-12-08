afc2b3657c4f1099d1f2fc462cfd0ef6f0c25253
Thanh Le <nhatthanhh1993@gmail.com>
2018-12-08 15:38:24 +0200

    user profile implementation complete and follower route updated

--- api/routes/follower.js
+++ api/routes/follower.js
@@ -1,18 +1,20 @@
 const router = require('express').Router();
 const upload = require('../../modules/multer');
 const db = require('../database/db');
-const passport = require('../../modules/passport-config');
 
 
 
 
-/* get all follower at GET: base_url/api/follower/ */
-router.get('/:user_id', db.Follower.getAllFollowers);
+/* get all people whom this user is Following to */
+router.get('/:user_id/following', db.Follower.getAllFollowing);
+
+/* get all people who follow this user */
+router.get('/:user_id/followed', db.Follower.getAllFollowed);
 
 /*add follower: base_url/api/follower/  */
-router.post('/:to_user_id', passport.authenticate('jwt', {session: false}), db.Follower.addFollower);
+router.post('/:to_user_id', db.Auth.authenticate, db.Follower.addFollower);
 
 /* delete follower by follower id: base_url/api/follower/:follower_id */
-router.delete('/:to_user_id', passport.authenticate('jwt', {session: false}), db.Follower.deleteFollower);
+router.delete('/:to_user_id', db.Auth.authenticate, db.Follower.deleteFollower);
 
 module.exports = router;
